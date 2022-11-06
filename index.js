227bfee930d2a6fb6f576244cb3c994eb34613f3
Theodore John Josef <john.josef@aol.com>
2022-11-06 01:04:16 -0800

    commit tree f7945361c859563d5ae3de369de577538563e64a

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'John Josef is the champion';
